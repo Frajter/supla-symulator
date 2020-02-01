8420a4cde6748a6aeabe6aaeff462c9ce50f6a7a
Grzegorz Kania <grzegorz69kania@gmail.com>
2020-02-01 17:06:45 +0100

    Dodano pozostałe pliki i plik EXE

---
+++ SuplaSym.dpr
@@ -0,0 +1,32 @@
+program SuplaSym;
+
+uses
+  Vcl.Forms,
+  Main in 'Main.pas' {MainForm},
+  Supla_proto in 'Supla_proto.pas',
+  SuplaDevDrvUnit in 'SuplaDevDrvUnit.pas',
+  SuplaDevUnit in 'SuplaDevUnit.pas' {SuplaDevForm},
+  ProgCfgUnit in 'ProgCfgUnit.pas',
+  ConfigEditUnit in 'ConfigEditUnit.pas' {ConfigForm},
+  NewChannelDialog in 'NewChannelDialog.pas' {NewChannelDlg},
+  BaseFrameUnit in 'Frames\BaseFrameUnit.pas' {BaseFrame: TFrame},
+  RelayFrameUnit in 'Frames\RelayFrameUnit.pas' {RelayFrame: TFrame},
+  TemperatureFrameUnit in 'Frames\TemperatureFrameUnit.pas' {TemperatureFrame: TFrame},
+  InputFrameUnit in 'Frames\InputFrameUnit.pas' {InputFrame: TFrame},
+  TempHumidityFrameUnit in 'Frames\TempHumidityFrameUnit.pas' {TempHumidityFrame: TFrame},
+  ElektrMeterFrameUnit in 'Frames\ElektrMeterFrameUnit.pas' {ElektrMeterFrame: TFrame},
+  ImpulsCounterFrameUnit in 'Frames\ImpulsCounterFrameUnit.pas' {ImpulsCounterFrame: TFrame},
+  ThermostatHeatpolFrameUnit in 'Frames\ThermostatHeatpolFrameUnit.pas' {ThermostatHeatpolFrame: TFrame},
+  ThermostatFrameUnit in 'Frames\ThermostatFrameUnit.pas' {ThermostatFrame: TFrame};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.CreateForm(TNewChannelDlg, NewChannelDlg);
+  Application.Run;
+end.
+
+
